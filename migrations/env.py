89fa15a4875a86f4ae7928f50861ab255da509ab
btambalo-dotcom <btambalo@gmail.com>
2025-09-28 12:55:29 -0400

    a

---
+++ migrations/env.py
@@ -0,0 +1,42 @@
+from __future__ import with_statement
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from logging.config import fileConfig
+from flask import current_app
+
+# Interpret the config file for Python logging.
+config = context.config
+
+# Set SQLAlchemy URL from the Flask app config
+config.set_main_option('sqlalchemy.url', str(current_app.config.get('SQLALCHEMY_DATABASE_URI')))
+
+target_metadata = current_app.extensions['migrate'].db.metadata
+
+def run_migrations_offline():
+    context.configure(
+        url=config.get_main_option('sqlalchemy.url'),
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()

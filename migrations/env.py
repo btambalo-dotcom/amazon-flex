e2e8de36b7aae283107a3ee3dabc8e43369cc22a
btambalo-dotcom <btambalo@gmail.com>
2025-09-28 15:39:40 -0400

    Add files via upload

--- migrations/env.py
+++ migrations/env.py
@@ -13,21 +13,17 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 config.set_main_option('sqlalchemy.url', str(current_app.config.get('SQLALCHEMY_DATABASE_URI')))
 
 def run_migrations_offline():
-    context.configure(
-        url=config.get_main_option("sqlalchemy.url"),
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
+    context.configure(url=config.get_main_option("sqlalchemy.url"),
+                      target_metadata=target_metadata,
+                      literal_binds=True,
+                      dialect_opts={"paramstyle": "named"})
     with context.begin_transaction():
         context.run_migrations()
 
 def run_migrations_online():
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = engine_from_config(config.get_section(config.config_ini_section),
+                                     prefix="sqlalchemy.",
+                                     poolclass=pool.NullPool)
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():

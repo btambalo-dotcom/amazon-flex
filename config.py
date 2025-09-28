059ba928a780c1c951e421354c68ee3dc2b63e39
btambalo-dotcom <btambalo@gmail.com>
2025-09-28 17:35:21 -0400

    a

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_FILE = os.getenv("DB_FILE", "flex.db")
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "secret-key")
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', DB_FILE)}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
